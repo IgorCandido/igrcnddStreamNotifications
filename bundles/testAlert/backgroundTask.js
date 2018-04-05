2c93c7b4787e7f571e04529d73d10d4441b4102b
igrcndd <igor.candido@judopayments.com>
2018-04-05 13:43:58 +0100

    Authentication with oauth to mixer
    Fetching of user info
    Authorized call to get viewers (not useful to what we need but at least guarantees that we are doing authenticated calls with right scope)

--- bundles/testAlert/backgroundTask.js
+++ bundles/testAlert/backgroundTask.js
@@ -1,3 +1,9 @@
+const Mixer = require('beam-client-node');
+const app = require('express')();
+const oauth2 = require('simple-oauth2')
+
+const client = new Mixer.Client(new Mixer.DefaultRequestRunner());
+
 module.exports = function(nodecg, Twitch){
 	var lastFollowRequest = nodecg.Replicant('lastFollowRequests', {defaultValue: "1970-01-01T00:00:00.000Z"});
 	var followOffset = nodecg.Replicant('followOffset', {defaultValue: 0});
@@ -84,6 +90,80 @@ module.exports = function(nodecg, Twitch){
 
 	// Show socials
 	setTimeout(PushSocials, periodforslides)
+
+	var oauthClient = oauth2.create({
+		client : {
+			id: '3836da539dc25dc874a72078c178daa2b438975665fd6e14'
+		},
+		auth: {
+		 tokenHost: 'https://mixer.com/',
+		 tokenPath: '/api/v1/oauth/token',
+		 authorizePath: '/oauth/authorize'
+	 },
+	 options: {
+        useBasicAuthorizationHeader: false,
+    }
+	});
+
+	const authorizationUri = oauthClient.authorizationCode.authorizeURL({
+	  redirect_uri: 'http://localhost:9090/testAlert/callback',
+	  scope: 'channel:analytics:self achievement:view:self',
+	  state: '3(#0/!~',
+	});
+
+	app.get('/testAlert/testAuth', (req, res) => {
+	  console.log(authorizationUri);
+	  res.redirect(authorizationUri);
+	});
+
+	app.get('/testAlert/callback', async (req, res) => {
+	  const code = req.query.code;
+	  const options = {
+			code: code,
+			redirect_uri: 'http://localhost:9090/testAlert/callback'
+	  };
+
+	  try {
+			console.log("getting token");
+
+	    const result = await oauthClient.authorizationCode.getToken(options);
+
+	    console.log('The resulting token: ', result);
+
+	    const token = oauthClient.accessToken.create(result);
+
+			client.use(new Mixer.OAuthProvider(client, {
+		    tokens: {
+		        access: token.token.access_token,
+		        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
+		    }
+			}));
+
+			client.request('GET', 'users/current')
+			.then(response => {
+			    console.log(response.body);
+					return response;
+			}).then(response =>{
+				console.log(response.body);
+				client.request('GET', `channels/${response.body.channel.id}/analytics/viewers?from=2018-04-04T00:00:00.000Z&to=2018-04-06T00:00:00.000Z`)
+				.then(res => {
+				    const body = JSON.stringify(res.body);
+				    console.log(`You have ${body} total viewers...`);
+				});
+			})
+
+
+
+	    return res.redirect("/dashboard")
+	  } catch(error) {
+	    console.error('Access Token Error', error.message);
+	    return res.status(500).json('Authentication failed');
+	  }
+	});
+
+
+	nodecg.mount(app);
+
 }
 
 
